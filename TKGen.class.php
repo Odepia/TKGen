569502a24a52cbc809f3322316ab343c358601a6
Chris Humbert <chris.humbert@odepia.com>
2013-09-09 16:25:12 -0400

    Update TKGen.class.php
    Small bug fixed

--- TKGen.class.php
+++ TKGen.class.php
@@ -12,7 +12,7 @@ class TKGen {
 	*/
 
 	public function token($token_id = NULL, $token_lenght = NULL) {
-		if (is_null($token_id) || strlen($token_id) > this->lenTK) {
+		if (is_null($token_id) || strlen($token_id) > $this->lenTK) {
 			$token = "0ab1cd2ef3gh4ij5km7no6pq8rs9tuvwxyz"; // Define a token work base value
 		} else {
 			$token = $token_id;                             // Use the value passed as argument
